b0499d0469214682156bc707ce1de814bf7ea092
Eddie Wang <eddie@eddiewang.me>
2012-09-21 22:17:19 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drubbi::Application.config.secret_token = 'a4c6c976946c90d0c5bf923912c422e159a3c4606fe7ec041c9d654c951f07791775f50d7b4dc1d0fcb5fbdda40500096278a7c74f30994e9f3bc703bc67d160'
